4a6d128cb2a10160412a00221cd89b1a08d6d6fd
Asteway <asteway.yiheyis@fdmgroup.com>
2024-09-18 12:00:55 -0400

    add user registration system

---
+++ src/main/java/exercises/io_exercise/user_regisration_system/User.java
@@ -0,0 +1,13 @@
+package exercises.io_exercise.user_regisration_system;
+
+public class User {
+    private String name;
+    private String address;
+    private String email;
+
+    public User(String name, String address, String email) {
+        this.name = name;
+        this.address = address;
+        this.email = email;
+    }
+}
--- src/main/java/exercises/io_exercise/user_regisration_system/UserRegistrationSystem.java
+++ src/main/java/exercises/io_exercise/user_regisration_system/UserRegistrationSystem.java
@@ -1,7 +1,11 @@
 package exercises.io_exercise.user_regisration_system;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class UserRegistrationSystem {
@@ -14,11 +18,38 @@ public class UserRegistrationSystem {
         System.out.print("Enter email: ");
         String email = scanner.nextLine();
 
-        try (FileWriter writer = new FileWriter("users_info.csv", true)) {
+        String fileName = "users_info.csv";
+
+        writeToFile(fileName, name, address, email);
+        List<User> users = readFromFileAndPopulate(fileName);
+
+        System.out.println(users);
+    }
+
+    private static void writeToFile(String fileName, String name, String address, String email) {
+        try (FileWriter writer = new FileWriter(fileName, true)) {
             writer.write(name + "," + address + "," + email + "\n");
-        }
-        catch (IOException ioException){
+        } catch (IOException ioException) {
             ioException.printStackTrace();
         }
+    }
+
+    private static List<User> readFromFileAndPopulate(String fileName) {
+        List<User> users = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split(",");
+                if (data.length == 3) {
+                    User user = new User(data[0], data[1], data[2]);
+                    users.add(user);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return users;
     }
 }
