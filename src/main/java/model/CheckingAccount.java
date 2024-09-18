2e42c7c2ca02d96c5d901daa1e651cd0c3cf1a89
Asteway <asteway.yiheyis@fdmgroup.com>
2024-09-18 14:48:15 -0400

    catch the exceptions in notify method

--- src/main/java/model/CheckingAccount.java
+++ src/main/java/model/CheckingAccount.java
@@ -3,11 +3,11 @@ package model;
 import service.AccountService;
 import service.AccountServiceImpl;
 import service.AlertService;
+import service.BankService;
 
 public class CheckingAccount extends Account {
     private int nextCheckNumber;
     private static final double MIN_BALANCE = 100.00;
-    AccountService accountService;
 
     public CheckingAccount(double balance, Customer customer){
         // it will start at 1 since the first call to getNextCheckNumber will return 1
@@ -26,13 +26,12 @@ public class CheckingAccount extends Account {
     @Override
     public double withdraw(double amount){
         double currentAmount = this.balance - amount;
-        accountService = new AccountServiceImpl(new Ac);
 
         if(currentAmount < MIN_BALANCE){
-            AlertService alertService = new AlertService();
+            BankService.notify(this, amount);
         }
 
-        this.balance -= amount;
+        this.balance = currentAmount;
 
         return amount;
     }
--- src/main/java/service/BankService.java
+++ src/main/java/service/BankService.java
@@ -4,21 +4,30 @@ import jakarta.mail.MessagingException;
 import model.Account;
 
 public class BankService {
-    private AlertService alertService;
+    private static AlertService alertService;
 
     public BankService(AlertService alertService){
         this.alertService = alertService;
     }
 
-    public void deposit(Account account, double amount) throws MessagingException {
+    public void deposit(Account account, double amount) {
         account.deposit(amount);
 
-        alertService.sendTransactionAlert(account.getCustomer().getCUSTOMER_ID(), account, amount);
+        BankService.notify(account, amount);
     }
 
-    public void withdraw(Account account, double amount) throws MessagingException {
+    public void withdraw(Account account, double amount) {
         account.withdraw(amount);
 
-        alertService.sendTransactionAlert(account.getCustomer().getCUSTOMER_ID(), account, amount);
+        BankService.notify(account, amount);
+    }
+
+    public static void notify(Account account, double amount) {
+        try{
+            alertService.sendTransactionAlert(account.getCustomer().getCUSTOMER_ID(), account, amount);
+        }
+        catch(MessagingException messagingException){
+            messagingException.printStackTrace();
+        }
     }
 }
